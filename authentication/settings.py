9185034ea22e43b9af294b7a403277f30f812bc4
Tushar Malhotra <officialtusharmalhotra@gmail.com>
2022-06-26 03:45:59 +0530

    Update settings.py

--- authentication/settings.py
+++ authentication/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp-relay.sendinblue.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER='tusharmalhotra494@gmail.com'
+EMAIL_HOST_USER=''
 EMAIL_HOST_PASSWORD='*************'
 
 
